74d453f69ca38bca3233907c17225acf98e1fe3a
Jmedal <784203082@qq.com>
2020-04-15 14:41:48 +0800

    2020-4-15 更新数据库文件

--- bdms/bdms.sql
+++ bdms/bdms.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2020-03-28 10:41:13
+Date: 2020-04-15 14:35:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -44,8 +44,8 @@ INSERT INTO `sys_menu` VALUES ('5', '1', '3', '课程', 'el-icon-data-board', '/
 INSERT INTO `sys_menu` VALUES ('6', '5', '1', '活跃度分析', 'el-icon-data-line', '/index/course_activity', '2', null, null, null);
 INSERT INTO `sys_menu` VALUES ('7', '5', '2', '学习情况分析', 'el-icon-pie-chart', '/index/course_study', '2', null, null, null);
 INSERT INTO `sys_menu` VALUES ('8', '5', '3', '排行榜', 'el-icon-trophy', '/index/course_list', '2', null, null, null);
-INSERT INTO `sys_menu` VALUES ('9', '1', '4', '学科', 'el-icon-collection', '/index4', '1', null, null, null);
-INSERT INTO `sys_menu` VALUES ('10', '9', '1', '热度分析', 'el-icon-data-line', '/index/curriculum_heat', '2', null, null, null);
+INSERT INTO `sys_menu` VALUES ('9', '1', '4', '科目', 'el-icon-collection', '/index4', '1', null, null, null);
+INSERT INTO `sys_menu` VALUES ('10', '9', '1', '结构分析', 'el-icon-connection', '/index/curriculum_heat', '2', null, null, null);
 INSERT INTO `sys_menu` VALUES ('11', '1', '5', '职业', 'el-icon-s-cooperation', '/index5', '1', null, null, null);
 INSERT INTO `sys_menu` VALUES ('12', '11', '1', '热门分析', 'el-icon-data-analysis', '/index/profession_heat', '2', null, null, null);
 INSERT INTO `sys_menu` VALUES ('13', '1', '6', '公司', 'el-icon-office-building', '/index6', '1', null, null, null);
@@ -69,14 +69,13 @@ INSERT INTO `sys_menu` VALUES ('30', '28', '2', '编辑菜单', 'el-icon-edit', 
 INSERT INTO `sys_menu` VALUES ('31', '28', '3', '删除菜单', 'el-icon-folder-delete', '/null', '3', null, null, null);
 INSERT INTO `sys_menu` VALUES ('32', '1', '8', 'Pholcus爬虫', 'el-icon-coffee-cup', '/index/pholcus', '1', null, null, null);
 INSERT INTO `sys_menu` VALUES ('33', '1', '9', '配置管理', 'el-icon-setting', '/index/config', '1', null, null, null);
-INSERT INTO `sys_menu` VALUES ('55', '1', '11', '测试菜单', 'el-icon-platform-eleme', '/vvv', '1', '', null, null);
-INSERT INTO `sys_menu` VALUES ('69', '1', '12', 'sadsad', 'asdasd', '/adssad', '1', '', null, null);
+INSERT INTO `sys_menu` VALUES ('55', '1', '11', '测试菜单', 'el-icon-platform-eleme', '/index/application', '1', '', null, null);
 INSERT INTO `sys_menu` VALUES ('70', '16', '4', '通知管理', 'el-icon-s-promotion', '/index/message', '2', '', null, null);
 INSERT INTO `sys_menu` VALUES ('71', '70', '1', '发布通知', 'el-icon-message', '/null', '3', '', null, null);
 INSERT INTO `sys_menu` VALUES ('72', '70', '3', '修改通知', 'el-icon-edit-outline', '/null', '3', '', null, null);
 INSERT INTO `sys_menu` VALUES ('73', '70', '4', '撤回通知', 'el-icon-refresh-left', '/null', '3', '', null, null);
 INSERT INTO `sys_menu` VALUES ('74', '17', '2', '更新用户状态', 'el-icon-open', '/null', '3', '', null, null);
-INSERT INTO `sys_menu` VALUES ('75', '70', '2', '切换通知置顶', 'el-icon-top', '/null', '3', '', null, null);
+INSERT INTO `sys_menu` VALUES ('75', '70', '2', '切换通知置顶状态', 'el-icon-top', '/null', '3', '', null, null);
 
 -- ----------------------------
 -- Table structure for sys_message
@@ -91,14 +90,14 @@ CREATE TABLE `sys_message` (
   `release_time` datetime(6) DEFAULT NULL COMMENT '发布时间',
   `is_top` int(2) unsigned DEFAULT '2' COMMENT '是否置顶（置顶：1，不置顶：2）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_message
 -- ----------------------------
-INSERT INTO `sys_message` VALUES ('18', '1', 'BDMS System 1.0上线', 'el-icon-info', '<h2><span style=\"color: rgb(230, 0, 0);\">系统部分</span></h2><p>包含用户管理，角色管理，权限管理，通知管理，</p><h2><span style=\"color: rgb(194, 133, 255);\">业务部分</span></h2><p>包含用户，课程，学科（未完成），职业，公司</p><h2><span style=\"color: rgb(255, 194, 102);\">爬虫部分（待完善，敬请期待）</span></h2>', '2020-03-27 22:48:39.000000', '1');
+INSERT INTO `sys_message` VALUES ('18', '1', 'BDMS System 1.0上线', 'el-icon-info', '<h2><span style=\"color: rgb(230, 0, 0);\">系统部分</span></h2><p>包含用户管理，角色管理，权限管理，通知管理，</p><h2><span style=\"color: rgb(194, 133, 255);\">业务部分</span></h2><p>包含用户，课程，学科（未完成），职业，公司</p><h2><span style=\"color: rgb(255, 194, 102);\">爬虫部分（待完善，敬请期待）</span></h2>', '2020-03-26 10:43:24.000000', '1');
 INSERT INTO `sys_message` VALUES ('19', '1', '测试', 'el-icon-warning', '<h2><span style=\"color: rgb(230, 0, 0);\">系统部分</span></h2><p><br></p><p>包含用户管理，角色管理，权限管理，通知管理，</p><p><br></p><h2><span style=\"color: rgb(194, 133, 255);\">业务部分</span></h2><p><br></p><p>包含用户，课程，学科（未完成），职业，公司，</p><p><br></p><h2><span style=\"color: rgb(255, 194, 102);\">爬虫部分（待完善，敬请期待）</span></h2><p><br></p>', '2020-03-18 00:00:00.000000', '2');
-INSERT INTO `sys_message` VALUES ('21', '1', '下一次更新', 'el-icon-info', '<h2><span style=\"color: rgb(230, 0, 0);\">下一次更新估计将在半个月后</span></h2><p><br></p><ol><li><em style=\"color: rgb(0, 102, 204);\">主要涉及爬虫模块的探索 和 与之匹配的可视化页面的实现</em></li><li><em style=\"color: rgb(0, 102, 204);\">其次还有学科模块的实现</em></li></ol>', '2020-03-28 00:03:59.000000', '2');
+INSERT INTO `sys_message` VALUES ('21', '1', '下一次更新', 'el-icon-info', '<h2><span style=\"color: rgb(230, 0, 0);\">下一次更新估计将在半个月后</span></h2><p><br></p><ol><li><em style=\"color: rgb(0, 102, 204);\">主要涉及爬虫模块的探索 和 与之匹配的可视化页面的实现</em></li><li><em style=\"color: rgb(0, 102, 204);\">其次还有学科模块的实现</em></li></ol>', '2020-03-27 00:03:59.000000', '2');
 
 -- ----------------------------
 -- Table structure for sys_message_user
@@ -110,7 +109,7 @@ CREATE TABLE `sys_message_user` (
   `user_id` bigint(255) unsigned DEFAULT NULL COMMENT '接收者id',
   `is_read` int(5) unsigned DEFAULT '1' COMMENT '阅读（1：未读，2：已读）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_message_user
@@ -163,7 +162,7 @@ CREATE TABLE `sys_role_menu` (
   `role_id` bigint(100) unsigned DEFAULT NULL COMMENT '角色id',
   `menu_id` bigint(80) unsigned DEFAULT NULL COMMENT '菜单id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=357 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=396 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_role_menu
@@ -259,10 +258,6 @@ INSERT INTO `sys_role_menu` VALUES ('314', '3', '11');
 INSERT INTO `sys_role_menu` VALUES ('315', '3', '15');
 INSERT INTO `sys_role_menu` VALUES ('316', '3', '10');
 INSERT INTO `sys_role_menu` VALUES ('317', '3', '9');
-INSERT INTO `sys_role_menu` VALUES ('318', '6', '33');
-INSERT INTO `sys_role_menu` VALUES ('319', '6', '55');
-INSERT INTO `sys_role_menu` VALUES ('320', '6', '32');
-INSERT INTO `sys_role_menu` VALUES ('321', '1', '69');
 INSERT INTO `sys_role_menu` VALUES ('322', '1', '55');
 INSERT INTO `sys_role_menu` VALUES ('323', '2', '33');
 INSERT INTO `sys_role_menu` VALUES ('324', '2', '14');
@@ -294,6 +289,7 @@ INSERT INTO `sys_role_menu` VALUES ('353', '1', '70');
 INSERT INTO `sys_role_menu` VALUES ('354', '1', '71');
 INSERT INTO `sys_role_menu` VALUES ('355', '1', '75');
 INSERT INTO `sys_role_menu` VALUES ('356', '2', '75');
+INSERT INTO `sys_role_menu` VALUES ('395', '6', '2');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -315,13 +311,14 @@ CREATE TABLE `sys_user` (
   `create_time` datetime(6) DEFAULT '2006-01-02 00:00:00.000000' COMMENT '创建时间',
   `version` int(11) DEFAULT NULL COMMENT '保留字段',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES ('1', 'null', 'root', 'root', '姜云杰', '2020-02-18 22:13:21.000000', '男', '784203082@qq.com', '18321882894', '1', '0', '1', '2020-02-18 23:52:21.000000', null);
-INSERT INTO `sys_user` VALUES ('2', 'null', 'roots', 'roots', '姜云杰', '1998-07-08 00:00:00.000000', '男', '784203082@qq.com', '18321882894', '1', '0', '2', '2020-03-23 00:00:00.000000', null);
+INSERT INTO `sys_user` VALUES ('2', 'null', 'roots', 'roots', '姜云杰', '1998-07-08 00:00:00.000000', '男', '784203082@qq.com', '18321882894', '1', '0', '1', '2020-03-23 00:00:00.000000', null);
 INSERT INTO `sys_user` VALUES ('4', 'null', 'sdadad', 'asdsadsad', 'asdasdasd', '2020-03-10 00:00:00.000000', '女', 'asdda@sd.com', '13821882894', '6', '0', '2', '2020-03-23 19:47:25.099788', null);
 INSERT INTO `sys_user` VALUES ('5', 'null', 'roots2', 'roots2', '姜云杰', '1998-07-08 00:00:00.000000', '男', '784203082@qq.ocm', '18321882894', '7', '0', '1', '2020-03-23 20:01:01.046430', null);
 INSERT INTO `sys_user` VALUES ('6', 'null', 'asdasdasd', 'sadadas', 'sadsa', '2020-03-02 00:00:00.000000', '女', 'asdasda@asasd.com', '18321882894', '6', '0', '2', '2020-03-23 20:17:27.970052', null);
+INSERT INTO `sys_user` VALUES ('13', 'null', 'liangping', 'liangping', '梁平', '1997-05-16 00:00:00.000000', '女', '6574561657@qq.com', '13888888888', '1', '0', '1', '2020-03-29 01:31:02.431545', null);
